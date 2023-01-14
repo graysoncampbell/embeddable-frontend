0bfb31c5b4f3d1ffe312254641382e040de0dfd2
Grayson Campbell <grayson@loonshoot.com>
2023-01-15 00:42:00 +1100

    Fix: changing class for className

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,36 +14,36 @@ import {
 export default function Header() {
   return (
     <header>
-      <div class="min-h-full border-solid border-b-2 border-light-blue-500">
-        <nav class="bg-white-800">
-          <div class="mx-auto max-w-7xl">
-            <div class="flex h-16 items-center">
-              <div class="flex items-center w-full justify-between">
-                <div class="flex-none items-center">
+      <div className="min-h-full border-solid border-b-2 border-light-blue-500">
+        <nav className="bg-white-800">
+          <div className="mx-auto max-w-7xl">
+            <div className="flex h-16 items-center">
+              <div className="flex items-center w-full justify-between">
+                <div className="flex-none items-center">
                   <Image width='35px' height='35px' src="/logo.svg" alt="Your Company" />
                 </div>
-                <div class="flex-auto w-10/12">
-                  <div class="ml-10 flex items-baseline space-x-10">
+                <div className="flex-auto w-10/12">
+                  <div className="ml-10 flex items-baseline space-x-10">
                     {navLinks.map((link, index) => {
                       return (
-                          <p class="text-sm font-medium hover:text-stone-700">
+                          <p className="text-sm font-medium hover:text-stone-700">
                             <Link href={link.path}>
-                              <span class="font-sans font-normal" key={index}>{link.name}</span>
+                              <span className="font-sans font-normal" key={index}>{link.name}</span>
                             </Link>
                           </p>
                       );
                     })}
                   </div>
                 </div>
-                <div class="flex-none">
-                  <div class="relative ml-3">
+                <div className="flex-none">
+                  <div className="relative ml-3">
                     <div>
                       <SelectOrganization />
                     </div>
                   </div>
                 </div>
-                <div class="flex-none">
-                  <div class="relative ml-3">
+                <div className="flex-none">
+                  <div className="relative ml-3">
                     <div>
                       <UserButton />
                     </div>
@@ -54,9 +54,9 @@ export default function Header() {
           </div>
         </nav>
       </div>
-      <div class="bg-white">
-        <div class="mx-auto max-w-7xl py-6">
-          <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+      <div className="bg-white">
+        <div className="mx-auto max-w-7xl py-6">
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
         </div>
       </div>
     </header>
