9d1884a5d72a6bcac3ce83d4993e5c302170cebd
Grayson Campbell <grayson@loonshoot.com>
2023-01-18 20:13:26 +1100

    Fix: Pending invites list only shows when users have been invited.

--- components/InvitationList.tsx
+++ components/InvitationList.tsx
@@ -10,24 +10,25 @@ export default function InvitationList() {
   const revoke = async (inv) => {
     await inv.revoke();
   };
-
-  return (
-    <div>
-      <div className="overflow-x-auto bg-white rounded-lg shadow overflow-y-auto relative my-10">
-        <div className="border-b">
-          <h2 className="text-lg font-medium px-6 py-3">Pending invitations</h2>
+  if (invitationList[0]) {
+    return (
+      <div>
+        <div className="overflow-x-auto bg-white rounded-lg shadow overflow-y-auto relative my-10">
+          <div className="border-b">
+            <h2 className="text-lg font-medium px-6 py-3">Pending invitations</h2>
+          </div>
+          <table className="border-collapse table-auto w-full whitespace-no-wrap bg-white table-striped relative">
+            <tbody className="text-left">
+              {invitationList.map((i) => (
+                <tr>
+                  <td className="px-6 py-3">{i.emailAddress}</td>
+                  <td className="px-6 py-3 text-right"><button className="py-3 underline underline-offset-2 decoration-solid text-red-600 rounded-md" onClick={() => revoke(i)}>Revoke invitation</button></td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-        <table className="border-collapse table-auto w-full whitespace-no-wrap bg-white table-striped relative">
-          <tbody className="text-left">
-            {invitationList.map((i) => (
-              <tr>
-                <td className="px-6 py-3">{i.emailAddress}</td>
-                <td className="px-6 py-3 text-right"><button className="py-3 underline underline-offset-2 decoration-solid text-red-600 rounded-md" onClick={() => revoke(i)}>Revoke invitation</button></td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
       </div>
-    </div>
-  );
+    );
+  }
 }
